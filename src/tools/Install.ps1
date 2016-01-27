050e1b9ac66a9b1b4469157f1ae5f9e6a7c4c47d
FantasticFiasco <mattias.kindborg@gmail.com>
2016-01-28 00:03:56 +0100

    Implemented configuring regarding Code Analysis when NuGet package is installed and uninstalled.

--- src/tools/Install.ps1
+++ src/tools/Install.ps1
@@ -1,6 +1,42 @@
 param($installPath, $toolsPath, $package, $project)
 
-## Custom Dictionary
-## Set build action to 'CodeAnalysisDictionary'
+###################################
+#        Before Installing        #
+###################################
+
+Write-Host ("Installing Visual Studio Ignition to project " + $project.FullName)
+$project.Save()
+
+###################################
+#        Custom Dictionary        #
+###################################
+
+Write-Host "Configuring build action on CustomDictionary.xml"
 $item = $project.ProjectItems.Item("CustomDictionary.xml")
 $item.Properties.Item("BuildAction").Value = [int]4
+
+
+###################################
+#          Code Analysis          #
+###################################
+
+# This is what we want to add to the project
+#  <PropertyGroup Label="VS-Ignition">
+#    <CodeAnalysisRuleSet>AllRules.ruleset</CodeAnalysisRuleSet>
+#    <RunCodeAnalysis>true</RunCodeAnalysis>
+#  </PropertyGroup>
+
+Write-Host "Configuring Code Analysis"
+$projectRootElement = [Microsoft.Build.Construction.ProjectRootElement]::Open($project.FullName)
+$propertyGroup = $projectRootElement.AddPropertyGroup()
+$propertyGroup.Label = "VS-Ignition"
+$propertyGroup.AddProperty('CodeAnalysisRuleSet', 'AllRules.ruleset')
+$propertyGroup.AddProperty('RunCodeAnalysis', 'true')
+$projectRootElement.Save()
+
+
+###################################
+#        After Installing         #
+###################################
+
+Write-Host "Installed Visual Studio Ignition"
--- src/tools/Uninstall.ps1
+++ src/tools/Uninstall.ps1
@@ -1,1 +1,41 @@
 param($installPath, $toolsPath, $package, $project)
+
+function RemoveCodeAnalysisPropertyGroups($projectRootElement) {
+    # If there are any PropertyGroups with a label of "VS-Ignition" they will be removed
+    $propertyGroupsToRemove = @()
+    
+    foreach($propertyGroup in $projectRootElement.PropertyGroups) {
+        if($propertyGroup.Label -and [string]::Compare("VS-Ignition", $propertyGroup.Label, $true) -eq 0) {
+            # Remove this property group
+            $propertyGroupsToRemove += $propertyGroup
+        }
+    }
+
+    foreach ($propertyGroup in $propertyGroupsToRemove) {
+        $propertyGroup.Parent.RemoveChild($propertyGroup)
+    }
+}
+
+###################################
+#       Before Uninstalling       #
+###################################
+
+Write-Host ("Uninstalling Visual Studio Ignition from project " + $project.FullName)
+$project.Save()
+
+
+###################################
+#          Code Analysis          #
+###################################
+
+Write-Host "Removing configuration of Code Analysis"
+$projectRootElement = [Microsoft.Build.Construction.ProjectRootElement]::Open($project.FullName)
+RemoveCodeAnalysisPropertyGroups -projectRootElement $projectRootElement
+$projectRootElement.Save()
+
+
+###################################
+#       After Uninstalling        #
+###################################
+
+Write-Host "Uninstalled Visual Studio Ignition"
