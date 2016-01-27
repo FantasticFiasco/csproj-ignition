059fa3970088795449293c3dde830d677b29d6c3
Mattias Kindborg <mattias.kindborg@gmail.com>
2016-01-27 22:15:16 +0100

    Implemented setting build action on the custom dictionary.

--- src/tools/Install.ps1
+++ src/tools/Install.ps1
@@ -1,1 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
+
+## Custom Dictionary
+## Set build action to 'CodeAnalysisDictionary'
+$item = $project.ProjectItems.Item("CustomDictionary.xml")
+$item.Properties.Item("BuildAction").Value = [int]4
