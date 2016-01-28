00e9802452e6a431658e2924252a401c923fcacc
Mattias Kindborg <mattias.kindborg@gmail.com>
2016-01-28 13:06:53 +0100

    Added signing to project when NuGet package is installed.

--- src/tools/Install.ps1
+++ src/tools/Install.ps1
@@ -6,37 +6,51 @@ param($installPath, $toolsPath, $package, $project)
 
 Write-Host ("Installing Visual Studio Ignition to project " + $project.FullName)
 $project.Save()
-
-###################################
-#        Custom Dictionary        #
-###################################
-
-Write-Host "Configuring build action on CustomDictionary.xml"
-$item = $project.ProjectItems.Item("CustomDictionary.xml")
-$item.Properties.Item("BuildAction").Value = [int]4
+$projectRootElement = [Microsoft.Build.Construction.ProjectRootElement]::Open($project.FullName)
 
 
 ###################################
 #          Code Analysis          #
 ###################################
 
+## Set 'Build Action' to 'CodeAnalysisDictionary' on custom dictionary
+Write-Host "Configuring build action on CustomDictionary.xml"
+$item = $project.ProjectItems.Item("CustomDictionary.xml")
+$item.Properties.Item("BuildAction").Value = [int]4
+
+## Enable code analysis on build and add ruleset
 # This is what we want to add to the project
 #  <PropertyGroup Label="Ignition">
-#    <CodeAnalysisRuleSet>AllRules.ruleset</CodeAnalysisRuleSet>
 #    <RunCodeAnalysis>true</RunCodeAnalysis>
+#    <CodeAnalysisRuleSet>AllRules.ruleset</CodeAnalysisRuleSet>
 #  </PropertyGroup>
 
 Write-Host "Configuring Code Analysis"
-$projectRootElement = [Microsoft.Build.Construction.ProjectRootElement]::Open($project.FullName)
 $propertyGroup = $projectRootElement.AddPropertyGroup()
 $propertyGroup.Label = "Ignition"
-$propertyGroup.AddProperty('CodeAnalysisRuleSet', 'AllRules.ruleset')
 $propertyGroup.AddProperty('RunCodeAnalysis', 'true')
-$projectRootElement.Save()
+$propertyGroup.AddProperty('CodeAnalysisRuleSet', 'AllRules.ruleset')
+
+
+###################################
+#             Signing             #
+###################################
+
+## Enable signing and specify strong name key file
+# This is what we want to update on the project
+#  <PropertyGroup Label="Ignition">
+#    <SignAssembly>true</SignAssembly>
+#    <AssemblyOriginatorKeyFile>StrongName.snk</AssemblyOriginatorKeyFile>
+#  </PropertyGroup>
+
+Write-Host "Configuring Signing"
+$propertyGroup.AddProperty('SignAssembly', 'true')
+$propertyGroup.AddProperty('AssemblyOriginatorKeyFile', 'StrongName.snk')
 
 
 ###################################
 #        After Installing         #
 ###################################
 
+$projectRootElement.Save()
 Write-Host "Installed Visual Studio Ignition"
